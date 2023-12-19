58c5a929f08f0beb4544f9053bd33042a4a53824
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2023-12-19 14:17:05 +0900

    太朗と花子の配置

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -11,13 +11,19 @@ class MainScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('background', 'assets/background.png');
+         this.load.image('background', 'assets/background.jpg');
+        this.load.image('taro', 'assets/taro.png');
+        this.load.image('hanako', 'assets/hanako.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(400, 300, 'background');
+         this.add.image(400, 300, 'background');
+         const taro = this.physics.add.sprite(500, 350, 'taro')
+         const hanako = this.physics.add.sprite(500, 350, 'hanako')
+         this.taro = taro
+         this.hanako = hanako
         }
 
      // 毎フレーム実行される繰り返し処理
