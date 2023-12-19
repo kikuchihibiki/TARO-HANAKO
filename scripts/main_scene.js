a7795fd8c8423fc9e102deb43b01942bc821447a
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2023-12-19 14:34:02 +0900

    太郎と花子を表示させた。カーソルキーで移動させた

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -1,35 +1,51 @@
-// シーンクラス
 // 他のJSファイルから呼び出された場合はシーンを返す
 class MainScene extends Phaser.Scene {
-    
     // コンストラクタ
     constructor() {
         // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
         super('MainScene');
     }
-
     // シーンの事前読み込み処理
     preload() {
-         // 画像の読み込み(使用する時の名前, パス)
-         this.load.image('background', 'assets/background.jpg');
-        this.load.image('taro', 'assets/taro.png');
-        this.load.image('hanako', 'assets/hanako.png');
-    }
-
-    // シーン初期化処理
-    create() {
-         // 単体画像をシーンに追加(X座標,Y座標,画像名)
-         this.add.image(400, 300, 'background');
-         const taro = this.physics.add.sprite(500, 350, 'taro')
-         const hanako = this.physics.add.sprite(500, 350, 'hanako')
-         this.taro = taro
-         this.hanako = hanako
-        }
-
+        // 画像の読み込み(使用する時の名前, パス)
+       this.load.image('background', 'assets/background.jpg');
+       this.load.image('taro', 'assets/taro.png');
+       this.load.image('hanako', 'assets/hanako.png');
+   }
+   // シーン初期化処理
+   create() {
+        // 単体画像をシーンに追加(X座標,Y座標,画像名)
+       this.add.image(400, 300, 'background');
+       const taro = this.physics.add.sprite(50, 50, 'taro')
+       const hanako = this.physics.add.sprite(300, 400, 'hanako')
+       this.taro = taro
+       this.hanako = hanako
+       }
      // 毎フレーム実行される繰り返し処理
-    update(time, delta) {
-        
+        update() {
+            // キーボードの情報を取得
+            let cursors = this.input.keyboard.createCursorKeys();
+            if(cursors.up.isDown){
+                console.log("Up!!");
+                this.taro.setVelocityY(-300);// 上方向の速度を設定
+                this.hanako.setVelocityY(300);// 上方向の速度を設定
+            } else if(cursors.down.isDown){
+                console.log("down!!");
+                this.taro.setVelocityY(300);// 下方向の速度を設定
+                this.hanako.setVelocityY(-300);// 下方向の速度を設定
+            }else if(cursors.left.isDown){
+                console.log("Left");
+                this.taro.setVelocityX(-300);// 左方向の速度を設定
+                this.hanako.setVelocityX(300);// 左方向の速度を設定
+            }else if(cursors.right.isDown){
+                console.log("Right!!");
+                this.taro.setVelocityX(300);// 右方向の速度を設定
+                this.hanako.setVelocityX(-300);// 右方向の速度を設定
+            }else{
+                this.taro.setVelocityX(0);// 横方向の速度を0
+                this.taro.setVelocityY(0);// 縦方向の速度を
+                this.hanako.setVelocityX(0);// 横方向の速度を0
+                this.hanako.setVelocityY(0);// 縦方向の速度を0
+            }
     }
-
 }
-
